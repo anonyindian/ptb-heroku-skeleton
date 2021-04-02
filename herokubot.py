f0ac2a0c19721f4d6fe75d0d680a63138c3b78d2
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2021-04-02 16:45:49 +0530

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     dp.add_error_handler(error)
 
     # Start the webhook
-    updater.start_webhook(listen="https://veer6860.herokuapp.com",
-                          port=int(8443),
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(5432),
                           url_path=TOKEN)
     updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
     updater.idle()
